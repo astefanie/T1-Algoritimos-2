9782ed967be677f695e9935c33d81ac38b884ece
stefanie <sstefanie103@gmail.com>
2026-09-04 09:36:01 -0300

    atualizações branch

--- JogoMatriz.cpp
+++ JogoMatriz.cpp
@@ -1,57 +1,64 @@
 #include <iostream>
 #include <windows.h>
+#define TAM 11
 using namespace std;
 
 
-
+void sobre(){ // Função do menu.
+    cout << "Equipe de desenvolvimento: " << endl
+         << "Stefanie Saffnauer" << endl
+         << "João Victor Atallah" << endl
+         << "Diogo Lucas" << endl
+         << "Setembro, 2026\n" << endl
+         << "Disciplina de Algoritimos II" << endl
+         << "Professor: Thiago Felski" << endl
+         << "W, A, S, D - movimenta para cima, esquerda, baixo e direita respectivamente" << endl     
+         << "Q - gira para esquerda" << endl
+         << "E - gira para direita" << endl                
+         << "R - reinicia a fase" << endl
+         << "ESC - volta ao menu" << endl 
+         << "Pressione enter para continuar: ";
+}
 
 int main(){
     SetConsoleCP(65001);
     SetConsoleOutputCP(65001);
-
     int option;
 
     do{
+        cout << "--- MENU ---" << endl
+             << "1 - Novo Jogo " << endl
+             << "2 - Continuar " << endl
+             << "3 - Sobre " << endl 
+             << "4 - Fim " << endl;
+             cout << "Escolha uma opção: " << endl;
+             cin >> option;
 
-    cout << "--- MENU ---" << endl
-         << "1 - Novo Jogo " << endl
-         << "2 - Continuar " << endl
-         << "3 - Sobre " << endl 
-         << "4 - Fim " << endl;
-         cout << "Escolha uma opção: " << endl;
-         cin >> option;
-
-         cout<<"\033c";
+             cout<<"\033c";
 
          
 
-    switch (option){
+        switch (option){
 
-        case 1:
-        break;
+            case 1:
+            break;
 
-        case 2:
-        break;
+            case 2:
+            break;
 
-        case 3:
-            cout << "Equipe de desenvolvimento: " << endl
-                 << "Stefanie Saffnauer" << endl
-                 << "João Victor Atallah" << endl
-                 << "Diogo Lucas" << endl
-                 << "Setembro, 2026\n" << endl
-                 << "Disciplina de Algoritimos II" << endl
-                 << "Professor: Thiago Felski" << endl;
+            case 3:
+                sobre();
                 cin.ignore();
                 cin.ignore();
                 cout<<"\033c";
-            break;
+                break;
 
-        case 4: 
-            cout << "Obrigado(a) por jogar!";
-            break;
+            case 4: 
+                cout << "Obrigado(a) por jogar!";
+                break;
 
-        default:
-            cout << "Opção invalida! ";
+            default:
+                cout << "Opção invalida! ";
 
 
     }
